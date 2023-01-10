6b625ce910aee1f4d0b197a7edaad5bb9bfb5619
ckangwen <2437037469@qq.com>
2023-01-10 15:47:42 +0800

    chore: 移除app

--- packages/app/src/layouts/default.tsx
+++
@@ -1,9 +0,0 @@
-import { GlobalLayout } from "@doain/client";
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "GlobalDefaultLayout",
-  render() {
-    return <GlobalLayout></GlobalLayout>;
-  },
-});
