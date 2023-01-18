ff806ce9941d3dcc906480a4c807e16c3ec93ec7
ckangwen <2437037469@qq.com>
2023-01-18 20:34:09 +0800

    fix(doain): fix a problem with doain command not working

---
+++ packages/doain/cli.ts
@@ -0,0 +1,1 @@
+import "@doain/node/dist/cli";
--- packages/doain/tsup.config.ts
+++ packages/doain/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["./index.ts"],
+  entry: ["./index.ts", "./cli.ts"],
   dts: true,
   clean: true,
   format: ["esm"],
